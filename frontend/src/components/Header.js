b996a4d763c4835c2efcde6d05b7366d0e54a8d1
hmhieu18 <hmhieu18@apcs.vn>
2022-01-18 21:01:54 +0700

    init

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Container, Row, Col, Nav, Navbar } from "react-bootstrap";
+
+function Header() {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">Car Dectection</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+}
+
+export default Header;
